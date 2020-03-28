5d401ed6d896c0dd98ea89e0611118b335e0176e
Артём <artyomd2009@yandex.ru>
2020-03-28 23:14:20 +0300

    добавлено(создание объявлений): отображение изображений
    При прикреплении фотографии, фотография отображается в списке прикрепленных
    Также фотографии теперь можно и удалять

--- src/panels/story/create/components/ChooseType.js
+++ src/panels/story/create/components/ChooseType.js
@@ -1,46 +1,46 @@
-import React, { useState, useEffect } from "react";
-import { Radio, Group, FormLayout, Header } from "@vkontakte/vkui";
+import React, { useState, useEffect } from 'react';
+import { Radio, Group, FormLayout, Header } from '@vkontakte/vkui';
 
 const ChooseAddType = props => {
-  return (
-    <div>
-      <Group separator="show" header={<Header mode="secondary">Отдаю</Header>}>
-        <FormLayout>
-          <div>
-            <Radio
-              onClick={e => {
-                props.set(e);
-              }}
-              name="radio"
-              value="1"
-              defaultChecked
-            >
-              Одну вещь
-            </Radio>
-            <Radio
-              onClick={e => {
-                props.set(e);
-              }}
-              name="radio"
-              value="2"
-            >
-              Несколько вещей одной категории
-            </Radio>
-            <Radio
-              name="radio"
-              onClick={e => {
-                props.set(e);
-              }}
-              value="3"
-              description="Для каждой вещи будет создано своё объявление"
-            >
-              Несколько вещей разных категорий
-            </Radio>
-          </div>
-        </FormLayout>
-      </Group>
-    </div>
-  );
+	return (
+		<div>
+			<Group separator="show" header={<Header mode="secondary">Отдаю</Header>}>
+				<FormLayout>
+					<div>
+						<Radio
+							onClick={e => {
+								props.set(e);
+							}}
+							name="radio"
+							value="1"
+							defaultChecked
+						>
+							Одну вещь
+						</Radio>
+						<Radio
+							onClick={e => {
+								props.set(e);
+							}}
+							name="radio"
+							value="2"
+						>
+							Несколько вещей одной категории
+						</Radio>
+						<Radio
+							name="radio"
+							onClick={e => {
+								props.set(e);
+							}}
+							value="3"
+							description="Для каждой вещи будет создано своё объявление"
+						>
+							Несколько вещей разных категорий
+						</Radio>
+					</div>
+				</FormLayout>
+			</Group>
+		</div>
+	);
 };
 
 export default ChooseAddType;
--- src/panels/story/create/components/CreateItem.js
+++ src/panels/story/create/components/CreateItem.js
@@ -1,190 +1,202 @@
-import React, { useState } from "react";
-import {
-  FormLayout,
-  Input,
-  Button,
-  Card,
-  CardGrid,
-  Div,
-  Textarea,
-  File,
-  SelectMimicry,
-  Gallery,
-  Header,
-  Group
-} from "@vkontakte/vkui";
+import React, { useState } from 'react';
+import { FormLayout, Input, Button, Card, CardGrid, Div, Textarea, File, HorizontalScroll } from '@vkontakte/vkui';
 
-import ImageGallery from "react-image-gallery";
+import { CategoriesLabel } from './../../../template/Categories';
 
-import {
-  Categories,
-  GetCategoryText,
-  CategoriesLabel
-} from "./../../../template/Categories";
+import Icon24Camera from '@vkontakte/icons/dist/24/camera';
 
-import Icon24Camera from "@vkontakte/icons/dist/24/camera";
-import Icon24Document from "@vkontakte/icons/dist/24/document";
-
-const nameLabel = "Название";
-const categoryLabel = "Категория";
-const descriptionLabel = "Описание";
+const nameLabel = 'Название';
+const categoryLabel = 'Категория';
+const descriptionLabel = 'Описание';
 
 const images = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/"
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/"
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/"
-  }
+	{
+		original: 'https://picsum.photos/id/1018/1000/600/',
+		thumbnail: 'https://picsum.photos/id/1018/250/150/',
+	},
+	{
+		original: 'https://picsum.photos/id/1015/1000/600/',
+		thumbnail: 'https://picsum.photos/id/1015/250/150/',
+	},
+	{
+		original: 'https://picsum.photos/id/1019/1000/600/',
+		thumbnail: 'https://picsum.photos/id/1019/250/150/',
+	},
 ];
 
 const CreateItem = props => {
-  const [name, setName] = useState(props.name);
-  const [description, setDescription] = useState("");
+	const [name, setName] = useState(props.name);
+	const [description, setDescription] = useState('');
+	const [photosUrl, setPhotosUrl] = useState([]);
 
-  return (
-    <CardGrid>
-      <Card size="l">
-        {props.len > 1 ? (
-          <Div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              padding: "5px"
-            }}
-          >
-            <Button
-              mode="destructive"
-              onClick={() => {
-                props.deleteMe();
-              }}
-            >
-              Удалить
-            </Button>
-          </Div>
-        ) : (
-          <div />
-        )}
+	function handleFileSelect(evt) {
+		var files = evt.target.files; // FileList object
+		// Loop through the FileList and render image files as thumbnails.
+		for (var i = 0, f; (f = files[i]); i++) {
+			// Only process image files.
+			if (!f.type.match('image.*')) {
+				alert('Image only please....');
+			}
+			var reader = new FileReader();
+			// Closure to capture the file information.
+			console.log('we are here');
+			reader.onload = (function(theFile) {
+				return function(e) {
+					setPhotosUrl([...photosUrl, e.target.result]);
+				};
+			})(f);
+			// Read in the image file as a data URL.
+			reader.readAsDataURL(f);
+		}
+	}
 
-        <Div
-          style={{
-            display: "flex",
-            alignItems: "flex-start",
-            padding: "0px"
-          }}
-        >
-          <FormLayout>
-            <Input
-              top={nameLabel}
-              name={nameLabel}
-              size="50"
-              placeholder="футбольный мяч"
-              value={name}
-              onChange={e => {
-                const { _, value } = e.currentTarget;
-                setName(value);
-                props.setItems({
-                  name: value,
-                  caregory: props.category,
-                  description,
-                  photos: props.item.photos
-                });
-              }}
-              status={name ? "valid" : "error"}
-            />
-          </FormLayout>
-          <CategoriesLabel category={props.category} open={props.choose} />
-        </Div>
-        <Div
-          style={{
-            padding: "0px"
-          }}
-        >
-          <FormLayout>
-            <Textarea
-              top={descriptionLabel}
-              name={descriptionLabel}
-              placeholder="Количество, состояние, габариты, дата покупки, особенности и т.д."
-              value={description}
-              onChange={e => {
-                const { _, value } = e.currentTarget;
-                setDescription(value);
-                props.setItems({
-                  name,
-                  category: props.category,
-                  description: value,
-                  photos: props.item.photos
-                });
-              }}
-              status={description ? "valid" : "error"}
-            />
-          </FormLayout>
-        </Div>
-        <FormLayout>
-          <File
-            top="Снимки вещей"
-            before={<Icon24Camera />}
-            size="l"
-            onChange={e => {
-              const file = e.target.files[0];
-              console.log("files", e.target.files);
-              //e.target.files.FileList.forEach(file => {
-              console.log("file:", file);
-              props.setItems({
-                name,
-                category: props.category,
-                description: description,
-                photos: [...props.item.photos, file]
-              });
-              // var reader = new FileReader();
-              // reader.readAsDataURL(file);
+	return (
+		<CardGrid>
+			<Card size="l">
+				{props.len > 1 ? (
+					<Div
+						style={{
+							display: 'flex',
+							justifyContent: 'flex-end',
+							padding: '5px',
+						}}
+					>
+						<Button
+							mode="destructive"
+							onClick={() => {
+								props.deleteMe();
+							}}
+						>
+							Удалить
+						</Button>
+					</Div>
+				) : (
+					<div />
+				)}
 
-              // reader.onloadend = function(e) {
-              //   props.setItems({
-              //     name,
-              //     category: props.category,
-              //     description: description,
-              //     photos: [...props.item.photos, reader.result]
-              //   });
-              // };
-
-              //});
-            }}
-          >
-            Открыть галерею
-          </File>
-          <Div
-            style={{
-              display: "flex",
-              alignItems: "flex-start",
-              padding: "0px"
-            }}
-          >
-            {props.item.photos.map(img => {
-              return (
-                <img
-                  src={img}
-                  style={{
-                    display: "flex",
-                    width: "10%",
-                    height: "10%"
-                  }}
-                />
-              );
-            })}
-            <img src={props.item.photos[0]} />
-            <ImageGallery items={images} />;
-          </Div>
-        </FormLayout>
-      </Card>
-    </CardGrid>
-  );
+				<Div
+					style={{
+						display: 'flex',
+						alignItems: 'flex-start',
+						padding: '0px',
+					}}
+				>
+					<FormLayout>
+						<Input
+							top={nameLabel}
+							name={nameLabel}
+							size="50"
+							placeholder="футбольный мяч"
+							value={name}
+							onChange={e => {
+								const { _, value } = e.currentTarget;
+								setName(value);
+								props.setItems({
+									name: value,
+									caregory: props.category,
+									description,
+									photos: props.item.photos,
+								});
+							}}
+							status={name ? 'valid' : 'error'}
+						/>
+					</FormLayout>
+					<CategoriesLabel category={props.category} open={props.choose} />
+				</Div>
+				<Div
+					style={{
+						padding: '0px',
+					}}
+				>
+					<FormLayout>
+						<Textarea
+							top={descriptionLabel}
+							name={descriptionLabel}
+							placeholder="Количество, состояние, габариты, дата покупки, особенности и т.д."
+							value={description}
+							onChange={e => {
+								const { _, value } = e.currentTarget;
+								setDescription(value);
+								props.setItems({
+									name,
+									category: props.category,
+									description: value,
+									photos: props.item.photos,
+								});
+							}}
+							status={description ? 'valid' : 'error'}
+						/>
+					</FormLayout>
+				</Div>
+				<FormLayout>
+					<File
+						top="Снимки вещей"
+						before={<Icon24Camera />}
+						size="l"
+						onChange={e => {
+							const file = e.target.files[0];
+							console.log('files', e.target.files);
+							console.log('file:', file);
+							props.setItems({
+								name,
+								category: props.category,
+								description: description,
+								photos: [...props.item.photos, file],
+							});
+							handleFileSelect(e);
+						}}
+					>
+						Открыть галерею
+					</File>
+					<Div
+						style={{
+							display: 'flex',
+							alignItems: 'flex-start',
+							padding: '0px',
+						}}
+					>
+						<HorizontalScroll>
+							<div style={{ display: 'flex' }}>
+								{photosUrl.map((img, i) => {
+									return (
+										<div
+											style={{
+												alignItems: 'center',
+												justifyContent: 'center',
+												padding: '10px',
+											}}
+										>
+											<img
+												key={i}
+												src={img}
+												style={{
+													height: '120px',
+												}}
+											/>
+											<Button
+												mode="destructive"
+												onClick={() => {
+													setPhotosUrl(photosUrl.splice(i, 1));
+													props.setItems({
+														name: name,
+														caregory: props.category,
+														description,
+														photos: [props.item.photos.splice(i, 1)],
+													});
+												}}
+											>
+												Удалить
+											</Button>
+										</div>
+									);
+								})}
+							</div>
+						</HorizontalScroll>
+					</Div>
+				</FormLayout>
+			</Card>
+		</CardGrid>
+	);
 };
 
 export default CreateItem;
